b84df7af191fe79c9af7cb24094ba42dcb3c1c0d
Aidan Skinner <aidan@litl.com>
2013-09-07 17:19:00 +0100

    manage: add setup command

--- taarifa_backend/manage.py
+++ taarifa_backend/manage.py
@@ -3,7 +3,10 @@ import os, sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from flask.ext.script import Manager, Server
+from mongoengine import ValidationError
+
 from taarifa_backend import app
+from taarifa_backend.models import clear_database, Role, User
 
 manager = Manager(app)
 
@@ -13,6 +16,24 @@ manager.add_command("runserver", Server(
     use_reloader = True,
     host = '0.0.0.0')
 )
+
+# Setup roles and initial admin user
+@manager.option("-e", "--email", dest="email")
+@manager.option("-c", "--clean", dest="clean", action="store_true", default=False)
+def setup(email, clean):
+    if clean:
+        clear_database()
+        
+    if email is None:
+        print "Please provide a valid email address for the admin"
+
+    role = Role(name="admin", description="Tarrifa Admin")
+    role.save()
+    
+    user = User()
+    user.email = email
+    user.roles = [role]
+    user.save()
 
 if __name__ == "__main__":
     manager.run()
