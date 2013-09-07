df34d30750b8ac35874cb238b0cd67f89f2eb2b1
Aidan Skinner <aidan@litl.com>
2013-09-07 15:48:30 +0100

    tests: add basic test framework and runner script

---
+++ run_tests.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+NOSETEST_OPTIONS="-s -d"
+
+if [ -n "$NOSE_PROCESSES" ]; then
+    NOCOLOR=1
+    unset COVERAGE
+else
+    NOSETEST_OPTIONS="$NOSETEST_OPTIONS --failed"
+fi
+
+if [ -n "$VERBOSE" ]; then
+    NOSETEST_OPTIONS="$NOSETEST_OPTIONS --verbose"
+fi
+
+if [ -n "$STOPFIRST" ]; then
+    NOSETEST_OPTIONS="$NOSETEST_OPTIONS --stop"
+fi
+
+NOSETEST_OPTIONS="$NOSETEST_OPTIONS $TESTS"
+
+nosetests $NOSETEST_OPTIONS 2>&1 | tee -a $OUTPUT_PATH/test.log
+
+exit ${PIPESTATUS[0]}
